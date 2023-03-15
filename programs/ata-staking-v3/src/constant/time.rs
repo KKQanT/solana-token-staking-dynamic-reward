0a1125fd30f01fdb741196346cac3719e8478ed0
Pain <peerakarn.jit@gmail.com>
2023-03-15 11:31:48 +0700

    mainnet config

--- programs/ata-staking-v3/src/constant/time.rs
+++ programs/ata-staking-v3/src/constant/time.rs
@@ -8,4 +8,4 @@ pub const VESTING_DURATION_3: i64 = 31556926 + 2629743 * 6; //1.5 years
 pub const VESTING_DURATION_4: i64 = 31556926 * 3; //3 years
 
 pub const EPOCH_DURATION: i64 = 86400*3; //3 days 
-pub const EPOCH_START_TS: i64 = 1677161921;
+pub const EPOCH_START_TS: i64 = 1678852876;
