8f44c8d73a1777b28201f2ad70e58eef1dfed64a
Pain <71139706+KKQanT@users.noreply.github.com>
2023-11-25 11:33:16 +0000

    Update address.rs

--- programs/token_staking/src/constant/address.rs
+++ programs/token_staking/src/constant/address.rs
@@ -1,4 +1,4 @@
-pub const EXPECTED_NFT_CREATOR_ADDRESS: &'static str = "CvHjwshHmbPfPGYBZpTiJqBZugAaZFksoUgoLs5wbduN";
-pub const ATA_TOKEN_ADDRESS: &'static str = "9cyEStsrZF7LzqLzbNcuUeuat1NM4eHrBVApvkPBCQk4";
-pub const PTT_TOKEN_ADDRESS: &'static str = "PTTWWFnxjqYoKMNCZsbpZbzix7gqw1fmcQp1wFfTfUF";
+pub const EXPECTED_NFT_CREATOR_ADDRESS: &'static str = "";
+pub const ATA_TOKEN_ADDRESS: &'static str = "";
+pub const PTT_TOKEN_ADDRESS: &'static str = "";
 pub const METADATA_PROGRAM_ID: &'static str = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s";
